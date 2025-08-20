c8814626a3c3e25e81e9540eeb895541bb0e5612
jeszDev <jjcr.dev@gmail.com>
2025-08-20 16:17:59 -0600

    build: recursos de pacientes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('patients', PatientController::class);
