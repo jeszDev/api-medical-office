aed179fdb22931c29441eb2800e269f8d22fc6f2
jeszDev <jjcr.dev@gmail.com>
2026-02-16 16:40:13 -0600

    feat: midleware sanctum

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/check-status', [AuthController::class, 'checkStatus'])->middleware('auth:sanctum');
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('patients', PatientController::class);
+Route::apiResource('patients', PatientController::class)->middleware('auth:sanctum');
 Route::apiResource('appointments', AppointmentController::class);
 Route::patch('appointments/{appointment}/cancel', [AppointmentController::class, 'cancel'])->name('appointments.cancel');
 
