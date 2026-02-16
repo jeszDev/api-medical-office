63ed39bbadc0eed08aab7bfba595f671221bb079
jeszDev <jjcr.dev@gmail.com>
2026-02-15 23:09:17 -0600

    feat: obtener los pacientes al recuparar las citas

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AppointmentResource;
 use App\Models\Appointment;
+use App\Models\CatalogAppointmentStatus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AppointmentController extends Controller
 {
@@ -13,7 +15,7 @@ class AppointmentController extends Controller
      */
     public function index(Request $request)
     {
-        $appointments = Appointment::whereBetween('fecha_hora_inicio', [$request->from_date, $request->to_date])->get();
+        $appointments = Appointment::with('patients')->whereBetween('fecha_hora_inicio', [$request->from_date, $request->to_date])->get();
         return AppointmentResource::collection($appointments);
     }
 
@@ -44,5 +46,18 @@ class AppointmentController extends Controller
     public function destroy(Appointment $appointment)
     {
         //
+    }
+
+    public function cancel(Appointment $appointment)
+    {
+        // if ($appointment->estatus === CatalogAppointmentStatus::CANCELADA) {
+        //     return response()->json(['message' => 'La cita ya está cancelada'], 422);
+        // }
+
+        Log::info('Cancelar cita', $appointment->toArray());
+
+        $appointment->cancel();
+
+        return new AppointmentResource($appointment);
     }
 }
